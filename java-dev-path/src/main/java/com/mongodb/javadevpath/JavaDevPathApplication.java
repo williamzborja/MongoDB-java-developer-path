f77b0521d6827732dda75d8544c451463a14d1e5
williamzborja <williamzborja@gmail.com>
2023-03-24 23:44:14 +0000

    init

---
+++ java-dev-path/src/main/java/com/mongodb/javadevpath/JavaDevPathApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb.javadevpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevPathApplication.class, args);
+	}
+
+}
---
+++ java-dev-path/src/test/java/com/mongodb/javadevpath/JavaDevPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodb.javadevpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
